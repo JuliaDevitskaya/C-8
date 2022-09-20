6d45bde8644b4b78836c6b98159ee2b011680d71
Julia <yagubetslulka@yandex.ru>
2022-09-20 15:43:28 +0300

    Задача1. Упорядочить по убыванию элементы каждой строки массива.

---
+++ 1.1/Program.cs
@@ -0,0 +1,68 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, 
+// которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+using static System.Console;
+using System.Linq;
+Clear();
+
+WriteLine("Введите параметры массива через пробел: ");
+int[] parametrs = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).ToArray();
+WriteLine();
+int[,] array = GetMatrix(parametrs[0], parametrs[1], parametrs[2], parametrs[3]);
+PrintMatrix(array);
+WriteLine();
+SortArray(array);
+PrintMatrix(array);
+
+
+int[,] GetMatrix(int rows, int colums, int minValue, int maxValue)
+{
+    int[,] result = new int[rows, colums];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < colums; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return result;
+}
+
+void PrintMatrix(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i, j]} ");
+        }
+        WriteLine();
+    }
+}
+
+void SortArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            for (int k = j + 1; k < inArray.GetLength(1); k++)
+            {
+                if (inArray[i, j] < inArray[i, k])
+                {
+                    int temp = inArray[i, j];
+                    inArray[i, j] = inArray[i, k];
+                    inArray[i, k] = temp;
+                }
+            }
+        }
+    }
+}
